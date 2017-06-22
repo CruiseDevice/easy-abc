d5d1e9fda0ef4e523bfc8c4d7a5ed4559397b3f4
Akash Chavan <meakashchavan@gmail.com>
2017-06-22 20:14:52 +0530

    done with html structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import EasyABC from './EasyABC';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2 className="easy-abc">Easy ABC</h2>
+          EasyABC
         </div>
-        <div className="game">
-          <h3>Random Letters</h3>
-          
-        </div>
+        <EasyABC/>
       </div>
     );
   }
---
+++ src/EasyABC.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class EasyABC extends Component {
+  render() {
+    return (
+      <div className = "game">
+        <div className ="option">
+          <div className = "fields">
+            <div className = "field-block">
+              A
+            </div>
+          </div>
+          <div className = "buttons">
+            <a href = "#" className = "button prev">Previous</a>
+
+            <a href = "#" className = "button sound">Play sound again</a>
+
+            <a href = "#" className = "button next">Next</a>
+          </div>
+          <div className = "fields">
+            <div className = "field-block">
+              <div className ="left-field">
+                  A
+              </div>
+              <div className = "right-field">
+                  B
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EasyABC;
